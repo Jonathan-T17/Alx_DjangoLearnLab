727b387b965f0eea61b283f500c803c43eedd013
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-09-25 19:33:58 +0100

    Functions, Data Structures and Modules based tests

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -37,7 +37,7 @@ def main():
             print("Invalid choice. Please enter 'C' or 'F'.")
 
     except ValueError:
-        # Handle invalid numeric input
+        # Handle invalid numeric user input
         print("Invalid temperature. Please enter a numeric value.")
 if __name__ == "__main__":
     main()
