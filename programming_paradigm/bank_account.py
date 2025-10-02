e276b2dff4943850d04c8bfa6b7df0675a2bfdf6
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-10-02 19:45:40 +0100

    Programming Paradigms & Exception handling

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -28,4 +28,4 @@ class BankAccount:
         
     # Print the current balance in a user-friendly format
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
