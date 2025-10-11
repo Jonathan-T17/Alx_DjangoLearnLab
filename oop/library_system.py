a7f2a90e194f391b21e2f3590fef4b487602e2ee
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-10-11 18:09:37 +0100

    Object-Oriented Programming (OOP) in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -38,7 +38,7 @@ class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book: Book):
+    def add_book(self, book):
         self.books.append(book)
     
     # def remove_book(self, book:Book):
