c3573f98279d2dc345a19f14b1a123c669ea434b
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-11-08 13:24:25 +0100

    Deep Dive into Django Models and Views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books, LibraryDetailView, register_view, add_book, edit_book, delete_book, admin_view, librarian_view, member_view
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    # path('register/', views.register_view, name='register'),
+    # path('login/', views.login_view, name='login'),
+    # path('logout/', views.logout_view, name='logout'),
 
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
