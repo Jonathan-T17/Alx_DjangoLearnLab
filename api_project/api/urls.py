322de78c550a0f9a4c65b7a6c264cd351481920f
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-11-19 00:33:14 +0100

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -11,5 +14,9 @@ urlpatterns = [
 
     # ViewSet router (CRUD)
     path('', include(router.urls)),  # Includes the routes from the BookViewSet
-]
 
+
+    # Token authentication endpoint
+    path('auth/token/', obtain_auth_token, name='api_token_auth'),
+    
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,15 +2,22 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 
 # Create your views here.
 
 # Existing ListAPIView
+# Public list: anyone logged in can view
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 # NEW ViewSet for CRUD
+# Full CRUD: only admins can create/update/delete
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
+
+
