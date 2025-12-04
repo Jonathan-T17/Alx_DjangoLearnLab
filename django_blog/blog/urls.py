124f26657b9fe7f8accdb481e0e9dd7955e2c6c3
Jonathan-T17 <twizerejonathan99@gmail.com>
2025-12-04 22:03:52 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
 
 
     # Comment URLs
-    path('post/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 ]
